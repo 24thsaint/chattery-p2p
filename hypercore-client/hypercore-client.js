a597bce33b845946025f2e730aa9f31a0fc4ebb4
24thsaint <admin@blackout.biz.tm>
2018-07-10 18:24:54 +0800

    feat(hypercore): Change keys and ids

--- hypercore-client/hypercore-client.js
+++ hypercore-client/hypercore-client.js
@@ -2,8 +2,8 @@ const hypercore = require('hypercore');
 const Swarm = require('../hypercore-swarm/index');
 var net = require('net');
 
-const feedId = '2621de13c958d61c7d7f652631e08b77de204082444534cc9fa94184fedb21c2';
-const feedDiscoveryKey = '88eccb72ae4fdffd2954612bf0d2ae90c5f4acf6ef816944455beaccf60d0cbd';
+const feedId = 'ce8e397a26cbfe5b75579a0f92617d6a16db7c6eacdfad21ea876b6284cc4975';
+const feedDiscoveryKey = '60f3312798ddb2cc734a1acaf9928f91f76b46fb48b04175726fc68fdffb28a2';
 
 const localFeed = hypercore('./messages', feedId, {
     valueEncoding: 'utf-8'
--- hypercore-server/hypercore-server.js
+++ hypercore-server/hypercore-server.js
@@ -1,8 +1,12 @@
 const hypercore = require('hypercore');
 const faker = require('faker');
 const Swarmer = require('../hypercore-swarm/index');
-const remoteFeed = hypercore('./messages', '2621de13c958d61c7d7f652631e08b77de204082444534cc9fa94184fedb21c2', {
-  valueEncoding: 'utf-8'
+
+const remoteFeed = hypercore('./messages', 'ce8e397a26cbfe5b75579a0f92617d6a16db7c6eacdfad21ea876b6284cc4975', {
+  createIfMissing: false, // create a new hypercore key pair if none was present in storage
+  overwrite: false, // overwrite any old hypercore that might already exist
+  valueEncoding: 'utf-8', // defaults to binary
+  storeSecretKey: true, // if false, will not save the secret key
 });
 
 const swarmer = new Swarmer();
@@ -25,6 +29,9 @@ var server = net.createServer(function (socket) {
 server.listen(6000);
 
 remoteFeed.on('ready', () => {
+  console.log(remoteFeed.key.toString('hex'))
+  console.log(remoteFeed.discoveryKey.toString('hex'))
+
   const key = remoteFeed.discoveryKey.toString('hex');
   swarmer.connect(key, (peer) => {
     console.log(`Connected to ${peer.address}:${peer.port}`)

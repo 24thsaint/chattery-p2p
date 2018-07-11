78a047a34bd4c926146d982d9c40e5f1b0024dfa
24thsaint <admin@blackout.biz.tm>
2018-07-11 13:55:15 +0800

    exp(Hypercore): New key

--- hyper-discovery/reader.js
+++ hyper-discovery/reader.js
@@ -1,7 +1,7 @@
 const hypercore = require("hypercore");
 const hyperdiscovery = require("hyperdiscovery");
 
-const remote = process.argv[2] || '6644879327d3aea348b24086e392e157105035a296640f1bd7ff05d70d275064';
+const remote = process.argv[2] || '41144b26d44dc50633811d7d3c1cd9773e0fea4cd2d7c3d59656397298dc201e';
 
 console.log(`Currently connecting to ${remote} hypercore`);
 console.log("Alternative usage: node hyperRead.js <key from writer.js|other hypercore key>");

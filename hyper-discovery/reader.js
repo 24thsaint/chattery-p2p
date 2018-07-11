5b68700bf855769b470f3257cd7801b1cab9aedb
24thsaint <admin@blackout.biz.tm>
2018-07-11 15:25:57 +0800

    exp(Hypercore): RAM impl revert

--- hyper-discovery/reader.js
+++ hyper-discovery/reader.js
@@ -1,12 +1,13 @@
 const hypercore = require("hypercore");
 const hyperdiscovery = require("hyperdiscovery");
+const ram = require('random-access-memory');
 
 const remote = process.argv[2] || '41144b26d44dc50633811d7d3c1cd9773e0fea4cd2d7c3d59656397298dc201e';
 
 console.log(`Currently connecting to ${remote} hypercore`);
 console.log("Alternative usage: node hyperRead.js <key from writer.js|other hypercore key>");
 
-const feed = hypercore('./data', remote, {
+const feed = hypercore(ram, remote, {
     valueEncoding: "json"
 });
 
